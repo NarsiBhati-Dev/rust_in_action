0b67b7512621f90fbe616493aebfbb92e366e27e
NarsiBhati-Dev <narsibhati31102@gmail.com>
2025-08-04 16:44:32 +0530

    learn

--- part_2_rust/src/main.rs
+++ part_2_rust/src/main.rs
@@ -1,25 +1,3 @@
-use std::{sync::mpsc, thread};
+// macros
 
-fn main() {
-    let (tx, rx) = mpsc::channel();
-
-    for i in 1..10 {
-        let producer = tx.clone();
-        thread::spawn(move || {
-            let mut ans: u64 = 0;
-            for j in 0..100000000 {
-                ans = ans + (i * 100000000 + j);
-            }
-            producer.send(ans).unwrap();
-        });
-    }
-
-    drop(tx);
-
-    let mut ans: u64 = 0;
-    for val in rx {
-        ans = ans + val;
-        println!("found value {val}");
-    }
-    println!("Ans is {ans}");
-}
+fn main() {}
--- part_2_rust/src/message_passing.rs
+++ part_2_rust/src/message_passing.rs
@@ -1,12 +1,38 @@
-use std::{sync::mpsc, thread::spawn};
+// use std::{sync::mpsc, thread::spawn};
+
+// fn main() {
+//     let (tx, rx) = mpsc::channel();
+//     spawn(move || tx.send(String::from("Hello,World")));
+
+//     let value = rx.recv();
+//     match value {
+//         Ok(value) => println!("{value}"),
+//         Err(err) => println!("error : {err}"),
+//     }
+// }
+
+use std::{sync::mpsc, thread};
 
 fn main() {
     let (tx, rx) = mpsc::channel();
-    spawn(move || tx.send(String::from("Hello,World")));
 
-    let value = rx.recv();
-    match value {
-        Ok(value) => println!("{value}"),
-        Err(err) => println!("error : {err}"),
+    for i in 1..10 {
+        let producer = tx.clone();
+        thread::spawn(move || {
+            let mut ans: u64 = 0;
+            for j in 0..100000000 {
+                ans = ans + (i * 100000000 + j);
+            }
+            producer.send(ans).unwrap();
+        });
     }
+
+    drop(tx);
+
+    let mut ans: u64 = 0;
+    for val in rx {
+        ans = ans + val;
+        println!("found value {val}");
+    }
+    println!("Ans is {ans}");
 }
